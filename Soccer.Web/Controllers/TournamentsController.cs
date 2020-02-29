95cfa3bd4c122aa65a3f939fd121257e20f344f8
JUAN DAVID GIL TAMAYO <juangil211237@correo.itm.edu.co>
2020-02-29 13:51:53 -0500

    Add Delete GroupDetails (Matches)

--- Soccer.Web/Controllers/TournamentsController.cs
+++ Soccer.Web/Controllers/TournamentsController.cs
@@ -343,6 +343,116 @@ namespace Soccer.Web.Controllers
             return View(model);
         }
 
+        public async Task<IActionResult> EditGroupDetail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var groupDetailEntity = await _context.GroupDetails
+                .Include(gd => gd.Group)
+                .Include(gd => gd.Team)
+                .FirstOrDefaultAsync(gd => gd.Id == id);
+            if (groupDetailEntity == null)
+            {
+                return NotFound();
+            }
+
+            var model = _converterHelper.ToGroupDetailViewModel(groupDetailEntity);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditGroupDetail(GroupDetailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var groupDetailEntity = await _converterHelper.ToGroupDetailEntityAsync(model, false);
+                _context.Update(groupDetailEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction($"{nameof(DetailsGroup)}/{model.GroupId}");
+            }
+
+            return View(model);
+        }
+        public async Task<IActionResult> EditMatch(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var matchEntity = await _context.Matches
+                .Include(m => m.Group)
+                .Include(m => m.Local)
+                .Include(m => m.Visitor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (matchEntity == null)
+            {
+                return NotFound();
+            }
+
+            var model = _converterHelper.ToMatchViewModel(matchEntity);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditMatch(MatchViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var matchEntity = await _converterHelper.ToMatchEntityAsync(model, false);
+                _context.Update(matchEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction($"{nameof(DetailsGroup)}/{model.GroupId}");
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> DeleteGroupDetail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var groupDetailEntity = await _context.GroupDetails
+                .Include(gd => gd.Group)
+                .FirstOrDefaultAsync(gd => gd.Id == id);
+            if (groupDetailEntity == null)
+            {
+                return NotFound();
+            }
+
+            _context.GroupDetails.Remove(groupDetailEntity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction($"{nameof(DetailsGroup)}/{groupDetailEntity.Group.Id}");
+        }
+
+        public async Task<IActionResult> DeleteMatch(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var matchEntity = await _context.Matches
+                .Include(m => m.Group)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (matchEntity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Matches.Remove(matchEntity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction($"{nameof(DetailsGroup)}/{matchEntity.Group.Id}");
+        }
+
 
     }
 }
